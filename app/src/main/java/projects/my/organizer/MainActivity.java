6492a3f0f3e105df0ae28c119abb60747825db6f
Aldo Venegas <aldo94vp@gmail.com>
2017-02-19 18:59:08 -0600

    Login activity created

--- app/src/main/java/projects/my/organizer/MainActivity.java
+++ app/src/main/java/projects/my/organizer/MainActivity.java
@@ -1,13 +1,24 @@
 package projects.my.organizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    LoginActivity loginActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (checkLogin()){
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean checkLogin() {
+        return true;
     }
 }
